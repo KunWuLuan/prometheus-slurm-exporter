42116eed55d299b1b84d28236a322440491e12ee
kunwuluan <kunwuluan@gmail.com>
2023-11-06 19:20:15 +0800

    support partition label in queue information

--- queue.go
+++ queue.go
@@ -16,66 +16,110 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus"
 	"io/ioutil"
 	"log"
 	"os/exec"
 	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type QueueMetrics struct {
-	pending     float64
-	pending_dep float64
-	running     float64
-	suspended   float64
-	cancelled   float64
-	completing  float64
-	completed   float64
-	configuring float64
-	failed      float64
-	timeout     float64
-	preempted   float64
-	node_fail   float64
+	pending     map[string]float64
+	pending_dep map[string]float64
+	running     map[string]float64
+	suspended   map[string]float64
+	cancelled   map[string]float64
+	completing  map[string]float64
+	completed   map[string]float64
+	configuring map[string]float64
+	failed      map[string]float64
+	timeout     map[string]float64
+	preempted   map[string]float64
+	node_fail   map[string]float64
 }
 
 // Returns the scheduler metrics
 func QueueGetMetrics() *QueueMetrics {
-	return ParseQueueMetrics(QueueData())
+	return ParseQueueMetrics(QueueData(), PartitionInfo())
 }
 
-func ParseQueueMetrics(input []byte) *QueueMetrics {
-	var qm QueueMetrics
+func PartitionInfo() []string {
+	cmd := exec.Command("sinfo", "--noheader", "-o %P")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	out, _ := ioutil.ReadAll(stdout)
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(out), "\n")
+	partitions := make([]string, len(lines))
+	for i, line := range lines {
+		partitions[i] = strings.TrimSpace(strings.TrimSuffix(line, "*"))
+	}
+	return partitions
+}
+
+func initMap(keys []string) map[string]float64 {
+	out := map[string]float64{}
+	for _, k := range keys {
+		out[k] = 0
+	}
+	return out
+}
+
+func ParseQueueMetrics(input []byte, partition []string) *QueueMetrics {
+	var qm QueueMetrics = QueueMetrics{
+		pending:     initMap(partition),
+		pending_dep: initMap(partition),
+		running:     initMap(partition),
+		suspended:   initMap(partition),
+		cancelled:   initMap(partition),
+		completing:  initMap(partition),
+		completed:   initMap(partition),
+		configuring: initMap(partition),
+		failed:      initMap(partition),
+		timeout:     initMap(partition),
+		preempted:   initMap(partition),
+		node_fail:   initMap(partition),
+	}
 	lines := strings.Split(string(input), "\n")
 	for _, line := range lines {
 		if strings.Contains(line, ",") {
 			splitted := strings.Split(line, ",")
 			state := splitted[1]
+			partition := splitted[3]
 			switch state {
 			case "PENDING":
-				qm.pending++
+				qm.pending[partition]++
 				if len(splitted) > 2 && splitted[2] == "Dependency" {
-					qm.pending_dep++
+					qm.pending_dep[partition]++
 				}
 			case "RUNNING":
-				qm.running++
+				qm.running[partition]++
 			case "SUSPENDED":
-				qm.suspended++
+				qm.suspended[partition]++
 			case "CANCELLED":
-				qm.cancelled++
+				qm.cancelled[partition]++
 			case "COMPLETING":
-				qm.completing++
+				qm.completing[partition]++
 			case "COMPLETED":
-				qm.completed++
+				qm.completed[partition]++
 			case "CONFIGURING":
-				qm.configuring++
+				qm.configuring[partition]++
 			case "FAILED":
-				qm.failed++
+				qm.failed[partition]++
 			case "TIMEOUT":
-				qm.timeout++
+				qm.timeout[partition]++
 			case "PREEMPTED":
-				qm.preempted++
+				qm.preempted[partition]++
 			case "NODE_FAIL":
-				qm.node_fail++
+				qm.node_fail[partition]++
 			}
 		}
 	}
@@ -84,7 +128,7 @@ func ParseQueueMetrics(input []byte) *QueueMetrics {
 
 // Execute the squeue command and return its output
 func QueueData() []byte {
-	cmd := exec.Command("squeue", "-a", "-r", "-h", "-o %A,%T,%r", "--states=all")
+	cmd := exec.Command("squeue", "-a", "-r", "-h", "-o %A,%T,%r,%P", "--states=all")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -107,18 +151,18 @@ func QueueData() []byte {
 
 func NewQueueCollector() *QueueCollector {
 	return &QueueCollector{
-		pending:     prometheus.NewDesc("slurm_queue_pending", "Pending jobs in queue", nil, nil),
-		pending_dep: prometheus.NewDesc("slurm_queue_pending_dependency", "Pending jobs because of dependency in queue", nil, nil),
-		running:     prometheus.NewDesc("slurm_queue_running", "Running jobs in the cluster", nil, nil),
-		suspended:   prometheus.NewDesc("slurm_queue_suspended", "Suspended jobs in the cluster", nil, nil),
-		cancelled:   prometheus.NewDesc("slurm_queue_cancelled", "Cancelled jobs in the cluster", nil, nil),
-		completing:  prometheus.NewDesc("slurm_queue_completing", "Completing jobs in the cluster", nil, nil),
-		completed:   prometheus.NewDesc("slurm_queue_completed", "Completed jobs in the cluster", nil, nil),
-		configuring: prometheus.NewDesc("slurm_queue_configuring", "Configuring jobs in the cluster", nil, nil),
-		failed:      prometheus.NewDesc("slurm_queue_failed", "Number of failed jobs", nil, nil),
-		timeout:     prometheus.NewDesc("slurm_queue_timeout", "Jobs stopped by timeout", nil, nil),
-		preempted:   prometheus.NewDesc("slurm_queue_preempted", "Number of preempted jobs", nil, nil),
-		node_fail:   prometheus.NewDesc("slurm_queue_node_fail", "Number of jobs stopped due to node fail", nil, nil),
+		pending:     prometheus.NewDesc("slurm_queue_pending", "Pending jobs in queue", []string{"partition"}, nil),
+		pending_dep: prometheus.NewDesc("slurm_queue_pending_dependency", "Pending jobs because of dependency in queue", []string{"partition"}, nil),
+		running:     prometheus.NewDesc("slurm_queue_running", "Running jobs in the cluster", []string{"partition"}, nil),
+		suspended:   prometheus.NewDesc("slurm_queue_suspended", "Suspended jobs in the cluster", []string{"partition"}, nil),
+		cancelled:   prometheus.NewDesc("slurm_queue_cancelled", "Cancelled jobs in the cluster", []string{"partition"}, nil),
+		completing:  prometheus.NewDesc("slurm_queue_completing", "Completing jobs in the cluster", []string{"partition"}, nil),
+		completed:   prometheus.NewDesc("slurm_queue_completed", "Completed jobs in the cluster", []string{"partition"}, nil),
+		configuring: prometheus.NewDesc("slurm_queue_configuring", "Configuring jobs in the cluster", []string{"partition"}, nil),
+		failed:      prometheus.NewDesc("slurm_queue_failed", "Number of failed jobs", []string{"partition"}, nil),
+		timeout:     prometheus.NewDesc("slurm_queue_timeout", "Jobs stopped by timeout", []string{"partition"}, nil),
+		preempted:   prometheus.NewDesc("slurm_queue_preempted", "Number of preempted jobs", []string{"partition"}, nil),
+		node_fail:   prometheus.NewDesc("slurm_queue_node_fail", "Number of jobs stopped due to node fail", []string{"partition"}, nil),
 	}
 }
 
@@ -152,18 +196,24 @@ func (qc *QueueCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- qc.node_fail
 }
 
+func (qc *QueueCollector) collect(metric *prometheus.Desc, ch chan<- prometheus.Metric, value map[string]float64) {
+	for k, v := range value {
+		ch <- prometheus.MustNewConstMetric(metric, prometheus.GaugeValue, v, k)
+	}
+}
+
 func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 	qm := QueueGetMetrics()
-	ch <- prometheus.MustNewConstMetric(qc.pending, prometheus.GaugeValue, qm.pending)
-	ch <- prometheus.MustNewConstMetric(qc.pending_dep, prometheus.GaugeValue, qm.pending_dep)
-	ch <- prometheus.MustNewConstMetric(qc.running, prometheus.GaugeValue, qm.running)
-	ch <- prometheus.MustNewConstMetric(qc.suspended, prometheus.GaugeValue, qm.suspended)
-	ch <- prometheus.MustNewConstMetric(qc.cancelled, prometheus.GaugeValue, qm.cancelled)
-	ch <- prometheus.MustNewConstMetric(qc.completing, prometheus.GaugeValue, qm.completing)
-	ch <- prometheus.MustNewConstMetric(qc.completed, prometheus.GaugeValue, qm.completed)
-	ch <- prometheus.MustNewConstMetric(qc.configuring, prometheus.GaugeValue, qm.configuring)
-	ch <- prometheus.MustNewConstMetric(qc.failed, prometheus.GaugeValue, qm.failed)
-	ch <- prometheus.MustNewConstMetric(qc.timeout, prometheus.GaugeValue, qm.timeout)
-	ch <- prometheus.MustNewConstMetric(qc.preempted, prometheus.GaugeValue, qm.preempted)
-	ch <- prometheus.MustNewConstMetric(qc.node_fail, prometheus.GaugeValue, qm.node_fail)
+	qc.collect(qc.pending, ch, qm.pending)
+	qc.collect(qc.pending_dep, ch, qm.pending_dep)
+	qc.collect(qc.running, ch, qm.running)
+	qc.collect(qc.suspended, ch, qm.suspended)
+	qc.collect(qc.cancelled, ch, qm.cancelled)
+	qc.collect(qc.completing, ch, qm.completing)
+	qc.collect(qc.completed, ch, qm.completed)
+	qc.collect(qc.configuring, ch, qm.configuring)
+	qc.collect(qc.failed, ch, qm.failed)
+	qc.collect(qc.timeout, ch, qm.timeout)
+	qc.collect(qc.preempted, ch, qm.preempted)
+	qc.collect(qc.node_fail, ch, qm.node_fail)
 }
